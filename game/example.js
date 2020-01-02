1fd931e3051538a64506ebe428ce1ea491503b90
James Michael Maltby <jamesmaltby@me.com>
2020-01-02 19:03:44 +0000

    --- Auto-Git Commit ---

--- game/example.js
+++ game/example.js
@@ -1,4 +1,5 @@
-let start = Room('start');
+
+let room = Room('start');
 
 let darkness = Hotspot('darkness', {
     x:"0%",
@@ -6,7 +7,8 @@ let darkness = Hotspot('darkness', {
     width:"100%",
     height:"80%",
     description:'Peer into darkness',
-    active:true
+    active:true,
+    flag:0
 });
 
 let moredarkness = Hotspot('moredarkness', {
@@ -15,16 +17,48 @@ let moredarkness = Hotspot('moredarkness', {
     width:"100%",
     height:"20%",
     description:'Inspect dark area at bottom of screen',
-    active:true
+    active:true,
+    flag:0
 });
 
 darkness.click = async function( hotspot, locals ) {
-    await Say('Hello darkness my old friend');
-    hotspot.description = 'Strain eyes to see in the dark';
+    switch( hotspot.flag ) {
+        case 0:
+            await Say('Hello darkness my old friend');
+            hotspot.description = 'Look closer into darkness';
+            break;
+        case 1:
+            await Say('BLACK PURE BLACKNESS!');
+            hotspot.description = 'Peer into blackness';
+            break;
+        case 2:
+            await Say('There must be something here that can help me');
+            hotspot.description = 'Peer into darkness';
+            break;
+    }
+    hotspot.flag++;
+    if( hotspot.flag>2 ) hotspot.flag=0;
 };
 
+moredarkness.click = async function( hotspot, locals ) {
+    switch( hotspot.flag ) {
+        case 0:
+            await Say( 'Hmmm what is that?');
+            hotspot.description = 'Look at strange object';
+            break;
+        case 1:
+            await Say( 'Strange it looks a bit like an object?');
+            hotspot.description = 'Ponder on the use of the strange object';
+            break;
+        case 2:
+            await Say( 'Nope no idea what it is!');
+            break;
+    }
+    hotspot.flag++;
+    if( hotspot.flag>2 ) hotspot.flag=1;
+};
 
-start.enter = async function( locals ) {
+room.enter = async function( locals ) {
     /*DisableInterface();
     await Say('Hello World!');
     await Say('This is an example of an enter script!');
@@ -32,6 +66,6 @@ start.enter = async function( locals ) {
     HotspotClick('start','darkness');*/
 };
 
-start.exit = async function( locals ) {
+room.exit = async function( locals ) {
 
 };
