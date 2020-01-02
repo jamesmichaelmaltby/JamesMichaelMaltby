59cf7251c8260dcd8c95b841b9d290352963a81c
James Michael Maltby <jamesmaltby@me.com>
2020-01-02 17:33:16 +0000

    --- Auto-Git Commit ---

--- game/example.js
+++ game/example.js
@@ -1,27 +1,28 @@
-NewRoom('start');
+let start = Room('start');
 
-NewHotspot('start','darkness', {
+let darkness = Hotspot('darkness', {
     x:0,
     y:0,
     width:100,
     height:100,
     description:'Peer into darkness',
     active:true
+});
+
+
+darkness.click = async function( hotspot, room ) {
+    await Say('Hello darkness my old friend');
 };
 
 
-rooms.start.hotspots.darkness.onclick = async function( hotspot, room, hotspots ) {
-
-}
-
-
-rooms.start.enter = async function( room, hotspots ) {
+start.enter = async function( room ) {
     DisableInterface();
     await Say('Hello World!');
     await Say('This is an example of an enter script!');
     EnableInterface();
+    HotspotClick('start','darkness');
 };
 
-rooms.start.exit = async function( room, locals, hotspots  ) {
+start.exit = async function( room ) {
 
 };
